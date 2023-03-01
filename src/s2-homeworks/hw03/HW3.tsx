13f536fb43d99a909c9d92976be3a44119c949b0
Daria Poddubnaia <sablezzubaya@gmail.com>
2023-03-02 00:05:45 +0300

    add: new task

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -23,7 +23,7 @@ export type UserType = {
     name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: string, setUsers: ([]) => void, users: UserType[]) => { // need to fix any
+export const pureAddUserCallback = (name: string, setUsers: (users: UserType[]) => void, users: UserType[]) => { // need to fix any
     const user = {_id: v1(), name: name}
     setUsers([...users, user])
     console.log(users)
--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,10 +18,17 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled
+            ? ' ' + s.disabled
+            : xType === 'red'
+                ? ' ' + s.red
+                : xType === 'secondary'
+                    ? ' ' + s.secondary
+                    : ' ' + s.default)
+        // ? (xType === 'red' ? className = s.disabled : '')
+        // : (xType === 'red' ? className = s.red : '')
+        //     ? (xType === 'secondary' ? className = s.secondary : '')
+        //     : (xType === '' ? className = s.default : ''))
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
